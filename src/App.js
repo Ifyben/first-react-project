7239f404a5ed1161ba7eb0e15bad1d1c856b262c
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-06-23 23:51:04 +0200

    I created an alert message

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MyNav from './components/MyNav';
 import MyFooter from './components/MyFooter';
+import Alert from './components/Alert';
 
 function App() {
   return (<>
     <MyNav />
     <MyFooter />
+    <Alert />
+
   </>);
 }
 
