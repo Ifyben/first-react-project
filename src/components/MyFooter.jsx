105759e6c517c24d900361a5c0354ce9eafcf23d
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-06-23 23:09:46 +0200

    I created a MyFooter component that will render a footer for the page.

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function MyFooter() {
+    return <footer className="d-flex bg-dark">
+        <span className="text-white m-auto"><strong>Beauty's World</strong> - Copyright 2022</span>
+    </footer>
+}
