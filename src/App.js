f213effd16617994d56780f7063e8bf0095e548a
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-06-24 11:08:50 +0200

    I created a Latest Release component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import MyNav from './components/MyNav';
 import MyFooter from './components/MyFooter';
 import Alert from './components/Alert';
+import LatestReleases from './components/LatestReleases';
 
 function App() {
   return (<>
     <MyNav />
     <MyFooter />
     <Alert />
-
+    <LatestReleases />
   </>);
 }
 
---
+++ src/components/LatestReleases.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+import fantasy from "../data/fantasy.json"
+
+export default function LatestReleases() {
+    console.log(fantasy)
+    return <div>Here are the latest releases</div>
+}
