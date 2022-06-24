bda7f51f39c78c5460388951d1c0f6174c9704aa
Beauty Azubuike <102667250+Ifyben@users.noreply.github.com>
2022-06-24 11:45:51 +0200

    Latest Release component read from one of the json files and printed out the cover of the books.

--- src/components/LatestReleases.jsx
+++ src/components/LatestReleases.jsx
@@ -1,8 +1,20 @@
 import React from "react"
-
+import Card from 'react-bootstrap/Card';
 import fantasy from "../data/fantasy.json"
 
 export default function LatestReleases() {
     console.log(fantasy)
-    return <div>Here are the latest releases</div>
+
+    return <div className="d-flex flex-wrap">{
+        fantasy.map( book => {  
+           //return <img className="book-cover" src={book.img} /> 
+              return <Card className="book-cover">
+                  <Card.Img variant="top" src={book.img} />
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                  </Card.Body>
+                </Card>  
+            
+        })
+        }</div>
 }
